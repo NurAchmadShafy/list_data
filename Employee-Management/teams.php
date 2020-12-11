e59210f3977efeb14b33420c0a8a2b2082be6faa
Nur Achmad Shafy Raissa Tsani <75611252+achmadxds@users.noreply.github.com>
2020-12-11 13:34:39 +0700

    update

--- Employee-Management/teams.php
+++ Employee-Management/teams.php
@@ -7,29 +7,62 @@
       q {
         font-style: italic;
       }
+      body {
+        background-color: #eae8e8;
+      }
+      .card-header {
+        background-color: #cac8c8;
+      }
+      .card {
+        border: .1px solid #444141;
+      }
     </style>
   </head>
-  <body>
 
-    <div class="container pt-5 text-center">
-        <div class="card-group">
-          <div class="card">
-            <div class="card-header text-center">
-              <h5><q> THE BIG THREE </q></h5>
-            </div>
-            <div class="card-body">
-              <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="img/mirio.jpg" alt="Card image cap">
+  <body>
+    <div class="container mt-5 pt-5">
+      <div class="card text-center">
+        <div class="card-header ">
+          <a href="index.php" class="card-text text-light"><q>THE BIG THREE</q></a>
+        </div>
+        <div class="card-body ">
+          <div class="container text-center d-flex justify-content-around">
+            <div class="row mr-5 pl-5">
+              <div class="card" style="width: 15rem;">
                 <div class="card-body">
-                  <h5 class="card-title text-danger"> NUR ACHMAD </h5>
-                  <q class="card-title">Flexible & Freakin' boy, but don't like anything shitty-bag activity</q>
+                  <img class="card-img-top" src="img/mirio.jpg">
+                  <div class="card-body">
+                    <p class="card-text text-primary">NUR ACHMAD</p>
+                    <q class="card-text">Some Freaking & Crazy boy!!</q>
                 </div>
               </div>
             </div>
+
+            <div class="row ml-5 pl-5">
+              <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                  <img class="card-img-top" src="img/hado.jpg">
+                  <div class="card-body">
+                  <p class="card-text text-primary">RIZKI INDAH</p>
+                  <q class="card-text">Some Freaking & Crazy boy!!</q>
+                </div>
+              </div>
+            </div>
+
+            <div class="row ml-5 pl-5">
+              <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                  <img class="card-img-top" src="img/tamaki.jpg">
+                  <div class="card-body">
+                    <p class="card-text text-primary">RASHYIF AZAZEL</p>
+                    <q class="card-text">Some Freaking & Crazy boy!!</q>
+                  </div>
+                </div>
+              </div>
           </div>
         </div>
     </div>
-
+    
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
